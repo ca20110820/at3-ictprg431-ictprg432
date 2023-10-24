b9e30c00da0d5c413baa53cff27a187090bf7c85
ca20110820 <ca20110820@gmail.com>
2023-10-24 16:58:10 +0800

    Branches User Control initial setup

--- AT3Project/UserControlTables/BranchesUserControl.xaml.cs
+++ AT3Project/UserControlTables/BranchesUserControl.xaml.cs
@@ -20,9 +20,54 @@ namespace AT3Project.UserControlTables
     /// </summary>
     public partial class BranchesUserControl : UserControl
     {
+
+        public MainWindow RootWindow
+        {
+            get
+            {
+                return Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            }
+        }
+
         public BranchesUserControl()
         {
             InitializeComponent();
+        }
+
+        private void buttonBranchAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonBranchUpdate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonBranchDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonBranchShowAll_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void datagridBranches_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            string header = e.Column.Header.ToString();
+
+            // Replace all underscores with two underscores, to prevent AccessKey handling
+            e.Column.Header = header.Replace("_", "__");
+
+            if (e.PropertyType == typeof(DateTime))
+                (e.Column as DataGridTextColumn).Binding.StringFormat = "dd/MM/yyyy HH:mm:ss";
+        }
+
+        private void datagridBranches_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
